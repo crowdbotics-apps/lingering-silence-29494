d3a7861cf86c51837f03c97827fa789c93b8d268
Crowdbotics <team@crowdbotics.com>
2021-08-05 03:37:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lingering_silence_29494Identifier",
+  url: "https://lingering_silence_29494Identifier.botics.co",
+  api: "https://lingering_silence_29494Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
